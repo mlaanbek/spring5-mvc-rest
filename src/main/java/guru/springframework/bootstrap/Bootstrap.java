3611a9daf1123d44c3f79df4f91c3914f4ef29d4
Marek Laanbek <marek.laanbek@gmail.com>
2020-04-21 16:48:01 +0300

    added bootstrap data

---
+++ src/main/java/guru/springframework/bootstrap/Bootstrap.java
@@ -0,0 +1,44 @@
+package guru.springframework.bootstrap;
+
+import guru.springframework.domain.Category;
+import guru.springframework.repositories.CategoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/* CommanLineRunner only works in SpringBoot. this class gets called on startup */
+
+@Component
+public class Bootstrap implements CommandLineRunner {
+
+    private CategoryRepository categoryRepository;
+
+    public Bootstrap(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Category fruits = new Category();
+        fruits.setName("Fruits");
+
+        Category dried = new Category();
+        fruits.setName("Dried");
+
+        Category fresh = new Category();
+        fruits.setName("Fresh");
+
+        Category exotic = new Category();
+        fruits.setName("Exotic");
+
+        Category nuts = new Category();
+        fruits.setName("Nuts");
+
+        categoryRepository.save(fruits);
+        categoryRepository.save(dried);
+        categoryRepository.save(fresh);
+        categoryRepository.save(exotic);
+        categoryRepository.save(nuts);
+
+        System.out.println("Data loaded = " + categoryRepository.count());
+    }
+}
